68b0fd4d33815837c61666c01275e3d523eb25e9
Alberto Carreras <acarrerasc@gmail.com>
2018-02-04 02:53:50 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,19 @@
+var chocolateBars = ["snickers","hundred grands","kitkat","skittles"]
+
+function addElementToBeginningOfArray(array,element) {
+  return [element, ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray(array,element) {
+  array = [element, ...array]
+  return array
+}
+
+function addElementoEndOfArray(array,element) {
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array,element) {
+  array = [...array, element]
+  return array
+}
