356021f2c333ecad22cf95bf4c393832da97b712
Alberto Carreras <acarrerasc@gmail.com>
2018-02-04 03:05:24 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ function addElementToBeginningOfArray(array,element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array,element) {
-  var array = [element, ...array]
+  array = [element, ...array]
   return array
 }
 
